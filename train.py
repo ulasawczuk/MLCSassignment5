5f1ab8ee406633c5fbf6e3f31bb4108c03430906
ulasawczuk <115264259+ulasawczuk@users.noreply.github.com>
2024-10-27 18:08:24 +0000

    final

--- train.py
+++ train.py
@@ -2,7 +2,6 @@ import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
 from tensorflow.keras.utils import to_categorical
-import numpy as np
 
 # MNIST dataset
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
